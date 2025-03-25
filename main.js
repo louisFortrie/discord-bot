e6a361abfe2ecdf398adad0eff53fc313aa3d8fa
louis fortrie <louis.fortrie4@gmail.com>
2025-03-25 19:56:05 +0100

    chore: initial commit :tada:

---
+++ main.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`Connecté en tant que ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("Pong!");
+  }
+});
+
+console.log("Lancement du bot...", process.env.TOKEN);
+
+client.login(process.env.TOKEN);

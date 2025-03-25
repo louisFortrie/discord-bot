eaf846d1ead4b67a2579851d6e2ee9d7962c90dc
louis fortrie <louis.fortrie4@gmail.com>
2025-03-25 21:25:49 +0100

    test

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
@@ -13,29 +14,27 @@ app.listen(PORT, () => {
   console.log(`Serveur web démarré sur le port ${PORT}`);
 });
 
-require("dotenv").config();
+// const client = new Client({
+//   intents: [
+//     GatewayIntentBits.Guilds,
+//     GatewayIntentBits.GuildMessages,
+//     GatewayIntentBits.MessageContent,
+//   ],
+// });
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
+// client.once("ready", () => {
+//   console.log(`Connecté en tant que ${client.user.tag}`);
+// });
 
-client.once("ready", () => {
-  console.log(`Connecté en tant que ${client.user.tag}`);
-});
+// client.on("messageCreate", (message) => {
+//   if (message.author.bot) return;
 
-client.on("messageCreate", (message) => {
-  if (message.author.bot) return;
+//   if (message.content === "!ping") {
+//     message.reply("Pong!");
+//   }
+// });
 
-  if (message.content === "!ping") {
-    message.reply("Pong!");
-  }
-});
-
-client.login(process.env.TOKEN).catch((err) => {
-  console.error("Erreur lors de la connexion :", err);
-  process.exit(1);
-});
+// client.login(process.env.TOKEN).catch((err) => {
+//   console.error("Erreur lors de la connexion :", err);
+//   process.exit(1);
+// });
